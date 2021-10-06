291367ff89bdb0873001f51f0b3f6bc00d4c47b1
Anthony R <70584761+MasterSSOA@users.noreply.github.com>
2021-10-06 01:00:52 -0400

    Versión 1

---
+++ Repository/IRepositorioProducto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Practica05.Core;
+using System.Collections.Generic;
+
+namespace Practica05.Repository
+{
+    public interface IRepositorioProducto
+    {
+        IEnumerable<Producto> GetProductosPorCategoria(int CategoriaId);
+        IEnumerable<Producto> GetProductosIntervalo(double Inicial, double Final);
+        IEnumerable<string> GetCategoriasRegistradas();
+    }
+}
